b1f771f50a055727aa6355efd011cf01a66d8c6a
Artur Araujo <artur.stat@gmail.com>
2021-10-29 12:53:11 +0100

    Update init.c

--- src/init.c
+++ src/init.c
@@ -21,9 +21,9 @@ static const R_CallMethodDef CallEntries[] = {
 void attribute_visible R_init_genSurv(DllInfo *dll) {
   R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
   R_useDynamicSymbols(dll, FALSE);
-#if defined(R_VERSION) && R_VERSION >= R_Version(2, 16, 0)
+  #if defined(R_VERSION) && R_VERSION >= R_Version(2, 16, 0)
   R_forceSymbols(dll, TRUE);
-#endif
+  #endif
 
   SEXP genSurv_NS = R_FindNamespace( mkString("genSurv") );
   if (genSurv_NS == R_UnboundValue) error("missing 'genSurv' namespace: should never happen");
