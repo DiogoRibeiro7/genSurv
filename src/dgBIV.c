f90bbe937808a2ba264d1aebaf5cdea14d1c3444
DiogoRibeiro7 <diogo_dj@hotmail.com>
2024-04-22 00:47:20 +0100

    chore: improvements

--- src/dgBIV.c
+++ src/dgBIV.c
@@ -4,58 +4,71 @@
 #include <Rmath.h>
 #include "defines.h"
 
-typedef void (*Tfunc)(CdoubleCP, CdoubleCP, doubleCP, doubleCP);
+// Define pointer types for clearer function signatures.
+typedef const double* CdoubleCP;
+typedef double* doubleCP;
 
-static void expt(
-  CdoubleCP pcorr,
-  CdoubleCP pdistpar,
-  doubleCP t1,
-  doubleCP t2)
-{
-  double u1, u2, v, a;
-  u1 = runif(0, 1);
-  v = runif(0, 1);
-  a = *pcorr*(2*u1-1);
-  u2 = 2*v/( 1-a+sqrt(R_pow_di(1-a, 2)+4*a*v) );
-  *t1 = -pdistpar[0]*log(1-u1);
-  *t2 = -pdistpar[1]*log(1-u2);
-  return;
-} // expt
+// Typedef for the distribution function pointer for improved readability.
+typedef void (*DistFunc)(CdoubleCP, CdoubleCP, doubleCP, doubleCP);
 
-static void weibullt(
-  CdoubleCP pcorr,
-  CdoubleCP pdistpar,
-  doubleCP t1,
-  doubleCP t2)
-{
-  register int i;
-  double u[5], v;
-  for (i = 0; i < 5; i++) u[i] = runif(0, 1);
-  if (u[4] > *pcorr) v = -log(u[3]);
-  else v = -log(u[1])-log(u[2]);
-  *t1 = R_pow(u[0], *pcorr/pdistpar[0])*R_pow(v, 1/pdistpar[0])*pdistpar[1];
-  *t2 = R_pow(1-u[0], *pcorr/pdistpar[2])*R_pow(v, 1/pdistpar[2])*pdistpar[3];
-  return;
-} // weibullt
+/**
+ * Exponential distribution function.
+ *
+ * @param pcorr Pointer to the correlation coefficient.
+ * @param pdistpar Pointer to the distribution parameters array.
+ * @param t1 Pointer to the first return variable.
+ * @param t2 Pointer to the second return variable.
+ */
+static void expt(CdoubleCP pcorr, CdoubleCP pdistpar, doubleCP t1, doubleCP t2) {
+    double u1 = runif(0, 1);
+    double v = runif(0, 1);
+    double a = *pcorr * (2 * u1 - 1);
+    double u2 = 2 * v / (1 - a + sqrt(pow(1 - a, 2) + 4 * a * v));
+    *t1 = -pdistpar[0] * log(1 - u1);
+    *t2 = -pdistpar[1] * log(1 - u2);
+}
 
-SEXP dgBIV(
-  SEXP n,
-  SEXP dist,
-  SEXP corr,
-  SEXP distpar)
-{
-  CcharCP pdist = CHAR( STRING_ELT(dist, 0) );
-  Tfunc tfunc = expt;
-  if (strcmp(pdist, "weibull") == 0) tfunc = weibullt;
-  else if (strcmp(pdist, "exponential") == 0) tfunc = expt;
-  SEXP mat;
-  PROTECT( mat = allocMatrix(REALSXP, *INTEGER(n), 2) );
-  register int i;
-  GetRNGstate();
-  for (i = 0; i < *INTEGER(n); i++) {
-    tfunc(REAL(corr), REAL(distpar), &REAL(mat)[i], &REAL(mat)[i+*INTEGER(n)]);
-  }
-  PutRNGstate();
-  UNPROTECT(1);
-  return(mat);
-} // dgBIV
+/**
+ * Weibull distribution function.
+ *
+ * @param pcorr Pointer to the correlation coefficient.
+ * @param pdistpar Pointer to the distribution parameters array.
+ * @param t1 Pointer to the first return variable.
+ * @param t2 Pointer to the second return variable.
+ */
+static void weibullt(CdoubleCP pcorr, CdoubleCP pdistpar, doubleCP t1, doubleCP t2) {
+    double u[5];
+    for (int i = 0; i < 5; i++) {
+        u[i] = runif(0, 1);
+    }
+    double v = (u[4] > *pcorr) ? -log(u[3]) : (-log(u[1]) - log(u[2]));
+    *t1 = pow(u[0], *pcorr / pdistpar[0]) * pow(v, 1 / pdistpar[0]) * pdistpar[1];
+    *t2 = pow(1 - u[0], *pcorr / pdistpar[2]) * pow(v, 1 / pdistpar[2]) * pdistpar[3];
+}
+
+/**
+ * Main function to generate a matrix of bivariate distributions.
+ *
+ * @param n Number of samples to generate.
+ * @param dist Distribution type ("weibull" or "exponential").
+ * @param corr Correlation coefficient.
+ * @param distpar Distribution parameters.
+ * @return SEXP Matrix of generated samples.
+ */
+SEXP dgBIV(SEXP n, SEXP dist, SEXP corr, SEXP distpar) {
+    const char* pdist = CHAR(STRING_ELT(dist, 0));
+    DistFunc tfunc = (strcmp(pdist, "weibull") == 0) ? weibullt : expt;
+
+    SEXP mat;
+    PROTECT(mat = allocMatrix(REALSXP, INTEGER(n)[0], 2));
+
+    GetRNGstate();
+    for (int i = 0; i < INTEGER(n)[0]; i++) {
+        tfunc(REAL(corr), REAL(distpar), &REAL(mat)[i], &REAL(mat)[i + INTEGER(n)[0]]);
+    }
+    PutRNGstate();
+
+    UNPROTECT(1);
+    return mat;
+}
+
--- src/init.c
+++ src/init.c
@@ -4,29 +4,35 @@
 #include <Rversion.h>
 #include "dgBIV.h"
 
-#define PREFIX Rf_
-
-#define STRINGIFY2(x) #x
-#define STRINGIFY(x) STRINGIFY2(x)
-#define PASTE2(a, b) a##b
-#define PASTE(a, b) PASTE2(a, b)
-
-#define CALLDEF(name, n)  {STRINGIFY(PASTE(PREFIX, name)), (DL_FUNC) &name, n}
+// Use straightforward macro definitions for clarity and simplicity.
+#define CALLDEF(name, n)  {#name, (DL_FUNC) &name, n}
 
 static const R_CallMethodDef CallEntries[] = {
-  CALLDEF(dgBIV, 4),
-  {NULL, NULL, 0}
+    CALLDEF(dgBIV, 4),
+    {NULL, NULL, 0} // Terminating entry
 };
 
+/**
+ * Initializes the genSurv package, registering routines and ensuring the
+ * package namespace is correctly identified.
+ *
+ * @param dll Pointer to the DLL information.
+ */
 void attribute_visible R_init_genSurv(DllInfo *dll) {
-  R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
-  R_useDynamicSymbols(dll, FALSE);
-  #if defined(R_VERSION) && R_VERSION >= R_Version(2, 16, 0)
-  R_forceSymbols(dll, TRUE);
-  #endif
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
 
-  SEXP genSurv_NS = R_FindNamespace( mkString("genSurv") );
-  if (genSurv_NS == R_UnboundValue) error("missing 'genSurv' namespace: should never happen");
-  if ( !isEnvironment(genSurv_NS) ) error("'genSurv' namespace not determined correctly");
-  return;
-} // R_init_genSurv
+    // Enforce symbol registration for R >= 2.16.0
+    #if defined(R_VERSION) && R_VERSION >= R_Version(2, 16, 0)
+    R_forceSymbols(dll, TRUE);
+    #endif
+
+    // Ensure the 'genSurv' namespace is correctly loaded and identified.
+    SEXP genSurv_NS = R_FindNamespace(mkString("genSurv"));
+    if (genSurv_NS == R_UnboundValue) {
+        error("Missing 'genSurv' namespace: should never happen.");
+    }
+    if (!isEnvironment(genSurv_NS)) {
+        error("'genSurv' namespace not determined correctly.");
+    }
+}
